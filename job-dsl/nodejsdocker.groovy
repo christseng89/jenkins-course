b88a2588a389a64d5a50d48f09581cdf09800ef5
Chris Tseng <samfire5200@gmail.com>
2017-12-07 19:12:18 +0800

    Using previous Docker Hub Credentails

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('christseng89/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('Docker Hub')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
